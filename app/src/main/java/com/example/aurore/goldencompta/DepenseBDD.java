69ce86ee328e0f41ffab7ea78184b0eb04993026
Aurore <rorosancho@hotmail.fr>
2016-11-16 10:36:49 +0100

    Finition de la base de données

---
+++ app/src/main/java/com/example/aurore/goldencompta/DepenseBDD.java
@@ -0,0 +1,81 @@
+package com.example.aurore.goldencompta;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by roros on 16/11/2016.
+ */
+
+public class DepenseBDD {
+
+    private static final int VERSION_BDD = 1;
+    private static final String NOM_BDD = "goldenCompta.db";
+
+    private static final String TABLE_DEPENSE = "table_depense";
+    private static final String COL_ID = "id";
+    private static final String COL_DATE = "date";
+    private static final String COL_CATEG = "categorie";
+
+    private SQLiteDatabase bdd;
+
+    private MaBaseSQLite maBaseSQLite;
+
+    public DepenseBDD(Context context){
+        //On crée la BDD et sa table
+        maBaseSQLite = new MaBaseSQLite(context, NOM_BDD, null, VERSION_BDD);
+    }
+
+    public void open(){
+        //on ouvre la BDD en écriture
+        bdd = maBaseSQLite.getWritableDatabase();
+    }
+
+    public void close(){
+        //on ferme l'accès à la BDD
+        bdd.close();
+    }
+
+    public SQLiteDatabase getBDD(){
+        return bdd;
+    }
+
+
+    public long insertDepense(Depense dep){
+        //Création d'un ContentValues (fonctionne comme une HashMap)
+        ContentValues values = new ContentValues();
+        //on lui ajoute une valeur associée à une clé (qui est le nom de la colonne dans laquelle on veut mettre la valeur)
+        values.put(COL_ID, dep.getId());
+
+        //Passage de la date en chaine de caractères
+        Format formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String s = formatter.format(dep.getDate());
+        values.put(COL_DATE, s);
+
+        values.put(COL_CATEG, dep.getCategorie().getNom());
+        //on insère l'objet dans la BDD via le ContentValues
+        return bdd.insert(TABLE_DEPENSE, null, values);
+    }
+
+    public int updateDepense(int id, Depense dep){
+        //La mise à jour d'une catégorie dans la BDD fonctionne plus ou moins comme une insertion
+        //il faut simplement préciser quel catégorie on doit mettre à jour grâce à l'ID
+        ContentValues values = new ContentValues();
+
+        Format formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String s = formatter.format(dep.getDate());
+        values.put(COL_DATE, s);
+
+        values.put(COL_CATEG, dep.getCategorie().getNom());
+        return bdd.update(TABLE_DEPENSE, values, COL_ID + " = " +id, null);
+    }
+
+    public int removeDepenseWithID(int id){
+        //Suppression d'une dépense de la BDD grâce à l'ID
+        return bdd.delete(TABLE_DEPENSE, COL_ID + " = " +id, null);
+    }
+}
--- app/src/main/java/com/example/aurore/goldencompta/MaBaseSQLite.java
+++ app/src/main/java/com/example/aurore/goldencompta/MaBaseSQLite.java
@@ -11,12 +11,20 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 
 public class MaBaseSQLite extends SQLiteOpenHelper {
 
-    private static final String TABLE_CATEGORIE = "table_categorie";
-    private static final String COL_ID = "id";
-    private static final String COL_NOM = "nom";
+    public  static final String TABLE_CATEGORIE = "table_categorie";
+    public  static final String COL_ID_CATEG = "id";
+    public  static final String COL_NOM = "nom";
 
-    private static final String CREATE_BDD = "CREATE TABLE " + TABLE_CATEGORIE + " ("
-            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NOM + " TEXT NOT NULL);";
+    public static final String TABLE_DEPENSE = "table_depense";
+    private static final String COL_ID_DEPENSE = "id";
+    private static final String COL_DATE = "date";
+    private static final String COL_CATEG = "categorie";
+
+
+    public  static final String CREATE_BDD = "CREATE TABLE " + TABLE_CATEGORIE + " ("
+            + COL_ID_CATEG + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NOM + " TEXT NOT NULL); " +
+            "CREATE TABLE "+ TABLE_DEPENSE + " (" + COL_ID_DEPENSE + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COL_DATE + " DATE NOT NULL, " + COL_CATEG + "); ";
 
     public MaBaseSQLite(Context context, String name, CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -33,6 +41,7 @@ public class MaBaseSQLite extends SQLiteOpenHelper {
         //On peut faire ce qu'on veut ici moi j'ai décidé de supprimer la table et de la recréer
         //comme ça lorsque je change la version les id repartent de 0
         db.execSQL("DROP TABLE " + TABLE_CATEGORIE + ";");
+        db.execSQL("DROP TABLE " + TABLE_DEPENSE + ";");
         onCreate(db);
     }
 }
