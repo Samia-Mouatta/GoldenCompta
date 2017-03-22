e8e417a87ab2a5e37f900156ca6b01cbbc56d972
Aurore <rorosancho@hotmail.fr>
2017-03-22 10:35:49 +0100

    BaseActivity :P

---
+++ app/src/main/java/com/example/aurore/goldencompta/BaseActivity.java
@@ -0,0 +1,154 @@
+package com.example.aurore.goldencompta;
+import android.content.Intent;
+import android.support.annotation.LayoutRes;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import static com.example.aurore.goldencompta.MainActivity.BUDGET;
+import static com.example.aurore.goldencompta.MainActivity.CAMERA;
+import static com.example.aurore.goldencompta.MainActivity.CATEGORIE;
+import static com.example.aurore.goldencompta.MainActivity.DEPENSE;
+import static com.example.aurore.goldencompta.MainActivity.IMAGE;
+
+public class BaseActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
+    private DrawerLayout fullLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle drawerToggle;
+    private int selectedNavItemId;
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        /**
+         * This is going to be our actual root layout.
+         */
+        fullLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+        /**
+         * {@link FrameLayout} to inflate the child's view. We could also use a {@link android.view.ViewStub}
+         */
+        FrameLayout activityContainer = (FrameLayout) fullLayout.findViewById(R.id.activity_content);
+        getLayoutInflater().inflate(layoutResID, activityContainer, true);
+
+        /**
+         * Note that we don't pass the child's layoutId to the parent,
+         * instead we pass it our inflated layout.
+         */
+        super.setContentView(fullLayout);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        if (useToolbar())
+        {
+            setSupportActionBar(toolbar);
+        }
+        else
+        {
+            toolbar.setVisibility(View.GONE);
+        }
+
+        setUpNavView();
+    }
+
+    /**
+     * Helper method that can be used by child classes to
+     * specify that they don't want a {@link Toolbar}
+     * @return true
+     */
+    protected boolean useToolbar()
+    {
+        return true;
+    }
+
+    protected void setUpNavView()
+    {
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if( useDrawerToggle()) { // use the hamburger menu
+            drawerToggle = new ActionBarDrawerToggle(this, fullLayout, toolbar,
+                    R.string.navigation_drawer_open,
+                    R.string.navigation_drawer_close);
+
+            fullLayout.setDrawerListener(drawerToggle);
+            drawerToggle.syncState();
+        } else if(useToolbar() && getSupportActionBar() != null) {
+            // Use home/back button instead
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(getResources()
+                    .getDrawable(R.drawable.ic_launcher));
+        }
+    }
+
+    /**
+     * Helper method to allow child classes to opt-out of having the
+     * hamburger menu.
+     * @return
+     */
+    protected boolean useDrawerToggle()
+    {
+        return true;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        fullLayout.closeDrawer(GravityCompat.START);
+        selectedNavItemId = menuItem.getItemId();
+
+        return onOptionsItemSelected(menuItem);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //Retour à la page d'accueil
+            case R.id.accueil:
+                Intent accueil = new Intent(this, MainActivity.class);
+                startActivity(accueil);
+                return true;
+            //case R.id.menu_about:
+            case R.id.tab_dep:
+                Intent tabDep = new Intent(this, TableauDepense.class);
+                startActivity(tabDep);
+                return true;
+            case R.id.menu_category:
+                // Comportement du bouton "Catégorie"
+                Intent intentCategory = new Intent(this, FormulaireCategorie.class);
+                startActivityForResult(intentCategory, CATEGORIE);
+                return true;
+            case R.id.menu_depense:
+                //Comportement du bouton "Dépense"
+                Intent intentDepense = new Intent(this, FormulaireDepense.class);
+                startActivityForResult(intentDepense, DEPENSE);
+                return true;
+            case R.id.menu_budget:
+                //Comportement du bouton "budget"
+                Intent intentBudget = new Intent(this, FormulaireBudget.class);
+                startActivityForResult(intentBudget, BUDGET);
+                return true;
+            case R.id.menu_statistique:
+                //Comportement du bouton "Stat"
+                Intent intentStat = new Intent(this, FormulaireStatistique.class);
+                startActivity(intentStat);
+                return true;
+            case R.id.menu_img:
+                Intent intentImage = new Intent(this, FormulaireImg.class);
+                startActivityForResult(intentImage, IMAGE);
+                return true;
+            case R.id.menu_camera:
+                Intent intentCamera = new Intent(this, FormulaireCamera.class);
+                startActivityForResult(intentCamera, CAMERA);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
